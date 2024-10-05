9bc4e272ced613b7afe64e39f017c89d852ab07f
Carson <carsonredacted@gmail.com>
2024-10-04 18:53:29 -0700

    buttongame 0.2

--- js/buttongame.js
+++ js/buttongame.js
@@ -2,15 +2,15 @@ const buttons = [
     new GameButton("key", "&#128273;", "game-body", addScore, 0),
     new GameButton("demon", "&#128520;", "game-body", addScore, 6.66), // Done.
     new GameButton("melon", "&#127817;", "game-body", addScore, 1.5),
-    new GameButton("shocked", "&#128576;", "game-body", addScore, -20),
-    new GameButton("ogre", "&#128121;", "game-body", setScore, -1000), // Done.
+    new GameButton("shocked", "&#128576;", "game-body", multScore, -0.5),
+    new GameButton("ogre", "&#128121;", "game-body", addScore, -999), // Done.
     new GameButton("poop", "&#128169;", "game-body", addScore, -1),
     new GameButton("puke", "&#129326;", "game-body", addScore, -5),
     new GameButton("diamond", "&#128142;", "game-body", addScore, 15), // Done.
     new GameButton("floppy", "&#128190;", "game-body", addScore, 0.5),
     new GameButton("mail", "&#128231;", "game-body", sendMail, 10),
-    new GameButton("bag", "&#128176;", "game-body", randScore, 100),
-    new GameButton("head", "&#128511;", "game-body", setScore, 333),
+    new GameButton("bag", "&#128176;", "game-body", randScore, 40),
+    new GameButton("head", "&#128511;", "game-body", rockHead, 111),
     new GameButton("shuffle", "&#128256;", "game-body", shuffleScore),
     new GameButton("dice", "&#127922;", "game-body", randScore, 6), // Done.
     new GameButton("lock", "&#128274;", "game-body", addScore, 0),
@@ -22,7 +22,7 @@ const buttons = [
     new GameButton("beers", "&#127867;", "game-body", addScore, 2),
     new GameButton("crazy", "&#129322;", "game-body", crazy, 200), // Done.
     new GameButton("donut", "&#127849;", "game-body", multScore, 0.01), //Done.
-    new GameButton("tophat", "&#127913;", "game-body", addScore, 7),
+    new GameButton("tophat", "&#127913;", "game-body", addScore, 8),
     new GameButton("sax", "&#127927;", "game-body", addScore, 3), // Done.
 ]
 
@@ -55,7 +55,7 @@ function GameButton(name, symbol, location, func, ...args){
 function addClick(){
     document.getElementById("click-value").innerHTML = parseFloat(document.getElementById("click-value").innerHTML) + 1;
     if (parseFloat(document.getElementById("score-value").innerHTML) >= 1000 && winstate == false) {
-        alert("YOU WIN!!!\n\nYou beat the game with " + document.getElementById("click-value").innerHTML + " clicks.\nCan you go lower?");
+        alert("YOU WIN!!!\n\nYou beat the game with " + document.getElementById("click-value").innerHTML + " clicks.\nCan you go lower? (It's possible in four clicks, two different ways!)");
         winstate = true;
     }
 }
@@ -80,6 +80,16 @@ function addScore(inc) {
         document.getElementById("score-value").innerHTML = 0;
     }
     deployScore(newscore);
+}
+
+let rockClick = false;
+function rockHead(inc){
+    if (rockClick == false){
+        let buttonElement = document.querySelector(`[name="head"]`);
+        buttonElement.value = decodeHtmlEntity("&#128168");
+        addScore(inc);
+        rockClick = true;
+    }
 }
 
 function multScore(inc){
@@ -148,6 +158,7 @@ function deployScore(newscore){
 function startGame(){
     winstate = false;
     crazyclicks = 1;
+    rockClick = false;
     document.getElementById("click-value").innerHTML = 0;
     document.getElementById("score-value").innerHTML = 0;
     buttons.forEach(button => button.create());
